d015c9e2fb23213cc3cff094a74c5fc5d5308d6f
matej <majtan.matej@gmail.com>
2019-10-22 21:18:13 +0200

    uploaded the files

---
+++ assignment2/Program.fs
@@ -0,0 +1,21 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+
+type Part = string
+type Task = string
+type Cost = int (* can be assumed to be positive *)
+type Duration = int (* can be assumed to be positive *)
+type PartReg = Map<Part, Cost>
+type TaskReg = Map<Task, Duration*Cost>
+(* Part and task registers for balance bikes *)
+let preg1 = Map.ofList [("wheel",50); ("saddle",10); ("handlebars",75); ("frame",100); ("screw bolt",5); ("nut",3)];;
+let treg1 = Map.ofList [("addWheels",(10,2)); ("addSaddle",(5,2)); ("addHandlebars",(6,1))]
+
+
+
+
+[<EntryPoint>]
+let main argv =
+    printfn "Hello World from F#!"
+    0 // return an integer exit code
